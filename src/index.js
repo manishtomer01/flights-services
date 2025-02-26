f2db2b7632a1a02609fffd5816b76091681adc51
Manish Tomer <mt0578902@gmail.com>
2025-02-26 14:07:57 +0530

    Added flights model with foreign key constraint defined during model defaination
    ```
    
    npx sequelize model:generate --name Flight --attributes airplaneId:integer,price:integer,flightNumber:string,departureAirportId:integer,arrivalAirportId:integer,arrivalTime:date,departureTime:date,boardingGate:string,totalSeats:number
    
    ```

--- src/index.js
+++ src/index.js
@@ -11,21 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, async () => {
+app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    // const city = await City.findByPk(2);
-    // const airport = await Airport.create({ name: "my airport", code: "MA" })
-    // console.log('airport :>> ', airport);
-    // const airport = await city.createAirport({ name: "my airport 2", code: "MA2" })
-    // console.log(airport)
-    // const airports = await city.getAirports()
-    // console.log(airports)
-    // const myAirport = await Airport.findByPk(3)
-    // console.log('myAirport >>', myAirport)
-    // await city.removeAirport(myAirport) it will not work because cityId can not be null
-    // await City.destroy({
-    //     where: {
-    //         id: 2
-    //     }
-    // })
 });
