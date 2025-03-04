deee8f0135bb12393be221844d27548e570fa076
Manish Tomer <mt0578902@gmail.com>
2025-03-04 10:38:37 +0530

    Added airport resource crud

--- src/config/logger-config.js
+++ src/config/logger-config.js
@@ -1,18 +1,18 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
 
-const customFormat = printf(( { level, message, timestamp, error } ) => {
+const customFormat = printf(({ level, message, timestamp, error }) => {
     return `${timestamp} : ${level}: ${message}`;
 });
 
 const logger = createLogger({
     format: combine(
-        timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+        timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
         customFormat,
     ),
     transports: [
         new transports.Console(),
-        new transports.File({filename: 'combined.log'})
+        new transports.File({ filename: 'combined.log' }),
     ],
 });
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 
-const { ServerConfig } = require('./config');
-const { City, Airport } = require('./models');
+const { ServerConfig, Logger } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
+
+app.use((req, res, next) => {
+    const { method, url, ip, headers, query, body } = req;
+
+    Logger.info(`📢 Request Received:
+    - Method: ${method}
+    - URL: ${url}
+    - IP: ${ip}
+    - Headers: ${JSON.stringify(headers)}
+    - Query Params: ${JSON.stringify(query)}
+    - Request Body: ${JSON.stringify(body)}
+    `);
+
+    // Capture response status code after response is sent
+    res.on('finish', () => {
+        Logger.info(`✅ Response Sent:
+        - Status Code: ${res.statusCode}
+        - Method: ${method}
+        - URL: ${url}
+        `);
+    });
+
+    next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
