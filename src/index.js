8efc058db9f182b7f568583d945d47278b28bf7d
Manish Tomer <mt0578902@gmail.com>
2025-03-14 17:41:54 +0530

    FEAT: added filters for get all flights

--- src/index.js
+++ src/index.js
@@ -5,29 +5,29 @@ const apiRoutes = require('./routes');
 
 const app = express();
 
-app.use((req, res, next) => {
-    const { method, url, ip, headers, query, body } = req;
+// app.use((req, res, next) => {
+//     const { method, url, ip, headers, query, body } = req;
 
-    Logger.info(`📢 Request Received:
-    - Method: ${method}
-    - URL: ${url}
-    - IP: ${ip}
-    - Headers: ${JSON.stringify(headers)}
-    - Query Params: ${JSON.stringify(query)}
-    - Request Body: ${JSON.stringify(body)}
-    `);
+//     Logger.info(`📢 Request Received:
+//     - Method: ${method}
+//     - URL: ${url}
+//     - IP: ${ip}
+//     - Headers: ${JSON.stringify(headers)}
+//     - Query Params: ${JSON.stringify(query)}
+//     - Request Body: ${JSON.stringify(body)}
+//     `);
 
-    // Capture response status code after response is sent
-    res.on('finish', () => {
-        Logger.info(`✅ Response Sent:
-        - Status Code: ${res.statusCode}
-        - Method: ${method}
-        - URL: ${url}
-        `);
-    });
+//     // Capture response status code after response is sent
+//     res.on('finish', () => {
+//         Logger.info(`✅ Response Sent:
+//         - Status Code: ${res.statusCode}
+//         - Method: ${method}
+//         - URL: ${url}
+//         `);
+//     });
 
-    next();
-});
+//     next();
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
