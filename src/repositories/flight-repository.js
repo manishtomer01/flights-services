f1f555a59ff9ed9ed37f7a85abd3e71c767ef9e8
Manish Tomer <mt0578902@gmail.com>
2025-04-11 14:40:12 +0530

    completed  flights api

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,5 +1,6 @@
 const CrudRepository = require('./crud-repository');
-const { Flight } = require('../models');
+const { Flight, Airplane, Airport, City } = require('../models');
+const { Sequelize } = require('sequelize');
 
 
 class FlightRepository extends CrudRepository {
@@ -10,7 +11,38 @@ class FlightRepository extends CrudRepository {
     async getAllFlights(filter, sort) {
         const response = await Flight.findAll({
             where: filter,
-            order: sort
+            order: sort,
+            include: [
+                {
+                    model: Airplane,
+                    required: true,
+                    as: 'airplaneDetail',
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'departureAirport',
+                    on: {
+                        col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
+                    },
+                    include: {
+                        model: City,
+                        required: true
+                    }
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'arrivalAirport',
+                    on: {
+                        col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
+                    },
+                    include: {
+                        model: City,
+                        required: true
+                    }
+                }
+            ]
         })
         return response
     }
